77118520944ac88bcda22393eb65068fec490bda
sahilgk-cms <sahil@catalysts.org>
2026-04-10 06:21:42 +0000

    refactor: filepaths

--- config/filepaths.py
+++ config/filepaths.py
@@ -8,11 +8,16 @@ SEARCH_SPACE_PATH = os.path.join(CURRENT_DIR, "config", "search_spaces.yml")
 VILLAGE_EMBEDDINGS_PATH = os.path.join(CURRENT_DIR, "embeddings", "emb_village.csv")
 
 
-TRAIN_PATH = "train_dataset.parquet"
-TEST_PATH = "test_dataset.parquet"
-FEATURE_IMPORTANCE_PATH = "feature_importance.parquet"
-PREDICTIONS_PATH = "predictions.parquet"
-SHAP_VALUES_PATH = "shap_values.parquet"
-SHAP_SUMMARY_PATH =  "shap_summary.png"
 
 
+
+ARTIFACTS_FOLDER = os.path.join(CURRENT_DIR, "artifacts")
+FEATURES_ARTIFACT = os.path.join(ARTIFACTS_FOLDER, "features.parquet")
+DATA_ARTIFACT = os.path.join(ARTIFACTS_FOLDER, "data.pkl")
+RUN_ARTIFACT = os.path.join(ARTIFACTS_FOLDER, "run_id.txt")
+TRAIN_PATH = os.path.join(ARTIFACTS_FOLDER, "train_dataset.parquet")
+TEST_PATH = os.path.join(ARTIFACTS_FOLDER, "test_dataset.parquet")
+FEATURE_IMPORTANCE_PATH = os.path.join(ARTIFACTS_FOLDER, "feature_importance.parquet")
+PREDICTIONS_PATH = os.path.join(ARTIFACTS_FOLDER, "predictions.parquet")
+SHAP_VALUES_PATH = os.path.join(ARTIFACTS_FOLDER, "shap_values.parquet")
+SHAP_SUMMARY_PATH =  os.path.join(ARTIFACTS_FOLDER, "shap_summary.png")
